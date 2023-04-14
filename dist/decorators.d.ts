5fbe53f8ed1b7357fdf10550a3cce5fe8fbb96fc
Bliiitz <benjamin@bliiitz.net>
2023-04-14 14:50:22 +0100

    wip

--- dist/decorators.d.ts
+++ dist/decorators.d.ts
@@ -1,1 +1,2 @@
 export declare function HandleEVMLog(logPattern: string): <TFunction extends Function, Y>(target: object | TFunction, propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<Y>) => void;
+export declare function HandleSyncState(pattern: string): <TFunction extends Function, Y>(target: object | TFunction, propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<Y>) => void;
--- dist/decorators.js
+++ dist/decorators.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HandleEVMLog = void 0;
+exports.HandleSyncState = exports.HandleEVMLog = void 0;
 const common_1 = require("@nestjs/common");
 const microservices_1 = require("@nestjs/microservices");
 function HandleEVMLog(logPattern) {
     return (0, common_1.applyDecorators)((0, microservices_1.MessagePattern)(logPattern));
 }
 exports.HandleEVMLog = HandleEVMLog;
+function HandleSyncState(pattern) {
+    return (0, common_1.applyDecorators)((0, microservices_1.MessagePattern)(pattern));
+}
+exports.HandleSyncState = HandleSyncState;
 //# sourceMappingURL=decorators.js.map
