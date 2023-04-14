e20e6e7b0b306514ba0f3864b789430b17dc4439
Bliiitz <benjamin@bliiitz.net>
2023-04-14 13:25:22 +0100

    wip

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,6 +1,5 @@
 import { applyDecorators } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
-import { ethers } from 'ethers';
 
 export function HandleEVMLog(logPattern: string) {
 	return applyDecorators(
